86f956b929ad3058ff013ff11e6e36c61a0c8fb1
niraj <nirajnsp5@gmail.com>
2023-08-06 20:20:19 +0530

    package json updated modal component basic structure adde

--- component/modal.el.js
+++ component/modal.el.js
@@ -1,16 +1,36 @@
-class Modal extends HTMLElement {
-    constructor(o) {
-        super();
-        this.options = o || {};
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = `
-        <h1>Modal Element</h1>
-        `;
+function define(o) {
+  class Modal extends HTMLElement {
+    constructor() {
+      super();
+      this.options = o || {};
+      this.attachShadow({ mode: 'open' });
+      this.shadowRoot.innerHTML = `
+            <h1>Modal Element</h1>
+            `;
     }
 
     connectedCallback() {
 
     }
+  }
+  return Modal
 }
+
+
 // customElements.define('tv-modal', Modal);0
-export { Modal }
+class ModalWrapper {
+  constructor(o) {
+    this._options = o || {};
+  }
+
+  init(o) {
+    //  Extend the options {o}
+    this.extends(o, this.options)
+    const _define = define(o);
+    const _name = 'tv-modal'
+    customElements.define(_name, _define);
+  }
+  extends(o, options) {
+    console.log({ o, options });
+  }
+}
